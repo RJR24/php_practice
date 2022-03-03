c8d3bfe578caea38fb9403a80f14e50224fe7e98
rjr24 <rezarezaeijamiuk@gmail.com>
2022-03-03 20:10:18 +0000

    and

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// declare(strict_types=1);
+
+// $list=[1,2,3,4,5,6,8,9,10];
+ 
+
+// function map ($array , $func) {
+//     $newList =[];
+//     foreach ($array as $key => $item) {
+//         $newList[$key] = $func($item);
+//     }
+//     return $newList;
+// }
+
+// function doS($num) {
+//     return $num * 2;
+// }
+
+// function doS2($num) {
+//     return $num ** 2; 
+// }
+// var_dump(map($list , 'doS2'));
+// var_dump (map($list , 'doS'));
+
+$c = 5;
+$sum = fn($a,$b) => $a+$b+$c;
+var_dump($sum(1,2));
---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+
+
+if (isset($_GET['username']) && isset($_GET['password'])) {
+
+$username = $_GET['username'];
+$password = $_GET['password'];
+
+var_dump($username,$password);
+} else {
+    echo 'username or password field is empty______';
+}
+
+var_dump($_POST);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+    <title>$_GET and $_POST</title>
+
+</head>
+
+<body>
+    <form action="/login.php" method='post'>
+
+        <?php if (isset($_POST['username'])) {?>
+
+            <h3>Hast dadash hast</h3>
+        <?php } ?>    
+        <input type="text" name="username">
+        <input type="password" name="password">
+        <button type="submit">send</button>
+
+    </form>
+</body>
+</html>
